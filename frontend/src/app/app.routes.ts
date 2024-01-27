976d0ddfcf747f40fd27e50faf0e825dd7c62c6d
Prateek Chaplot <prateekchaplot96@gmail.com>
2024-01-27 19:22:20 +0530

    Add basic layout

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './core/login/login.component';
+import { RegisterComponent } from './core/register/register.component';
+import { HomeComponent } from './core/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'login',
+        component: LoginComponent
+      },
+      {
+          path: 'register',
+          component: RegisterComponent
+      },
+      {
+          path: '',
+          component: HomeComponent
+      }
+];
